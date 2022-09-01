1f76fec6985bed7e7af88e8f78a5e7fb57e381bb
shanek16 <shane16@naver.com>
2022-09-02 00:02:37 +0900

    before setting fitness as mfar=1.0

--- evolve_multiGPU.sh
+++ evolve_multiGPU.sh
@@ -1,5 +1,5 @@
 for i in 0 1 2 3; do
   sleep $(expr 30 \* $i) &&  # 30-second delay (optional)
   echo 'Starting GPU '$i'...' &&
-  nohup python train.py --img 640 --bath 32 --epochs 10 --data safety.yaml --weights L_best.pt --cache --device $i --evolve > evolve_gpu_$i.log &
+  nohup python train.py --img 640 --bath 32 --epochs 10 --data safety.yaml --weights yolov5l.pt --cache --device $i --evolve > evolve_gpu_$i.log &
 done
